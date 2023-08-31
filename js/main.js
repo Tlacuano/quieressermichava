300cbbecc0e7e66d89fff5c5fc25deb61715b9aa
Tlacuano <105244719+Tlacuano@users.noreply.github.com>
2023-08-31 15:13:01 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const noButton = document.querySelector("#no");
+
+    noButton.addEventListener("mouseover", function() {
+        const maxX = window.innerWidth - noButton.clientWidth;
+        const maxY = window.innerHeight - noButton.clientHeight;
+
+        const randomX = Math.floor(Math.random() * maxX);
+        const randomY = Math.floor(Math.random() * maxY);
+
+        noButton.style.position = "absolute";
+        noButton.style.left = `${randomX}px`;
+        noButton.style.top = `${randomY}px`;
+    });
+});
+  
+
